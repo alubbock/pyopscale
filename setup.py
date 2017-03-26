a292b008dca3691521ce7a06f0dcd9e94c0d26ad
Alex Lubbock <code@alexlubbock.com>
2017-03-26 18:05:13 -0500

    Add setup.py for easier installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pyopscale',
+      version='1.0',
+      description='Optimal scaling in Python',
+      author='Alex Lubbock, William G. Jacoby',
+      author_email='code@alexlubbock.com',
+      url='https://github.com/alubbock/pyopscale',
+      packages=['pyopscale'],
+      install_requires=['rpy2']
+     )
